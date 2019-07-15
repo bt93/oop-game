70a6965a1d56b43926df30066ea09434099c8eac
Jason Howie <howiejason93@gmail.com>
2019-07-14 21:30:27 -0400

    create addPhraseToDisplay

--- js/Phrase.js
+++ js/Phrase.js
@@ -5,4 +5,22 @@ class Phrase {
     constructor(phrase) {
         this.phrase = phrase.toLowerCase();
     }
+
+    /**
+     * Display the phrase 
+     */
+    addPhraseToDisplay() {
+        const phraseHolder = document.querySelector('#phrase ul');
+        const splitPhrase = this.phrase.split('');
+        splitPhrase.forEach(letter => {
+            let li = document.createElement('li');
+            li.textContent = letter;
+            if (letter === ' ') {
+                li.className = 'space'
+            } else {
+                li.className = `hide letter ${letter}`;
+            }
+            phraseHolder.appendChild(li);
+        });
+    }
 }
