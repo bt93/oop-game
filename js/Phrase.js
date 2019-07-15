2eb8f5cc8cdc5d484ae874a2c3ad6c594036f00a
Jason Howie <howiejason93@gmail.com>
2019-07-15 11:33:17 -0400

    create showMAtchedLetter

--- js/Phrase.js
+++ js/Phrase.js
@@ -13,8 +13,10 @@ class Phrase {
         const phraseHolder = document.querySelector('#phrase ul');
         const splitPhrase = this.phrase.split('');
         splitPhrase.forEach(charater => {
+            // create new li and add charater
             let li = document.createElement('li');
             li.textContent = charater;
+            // check if empty string
             if (charater === ' ') {
                 li.className = 'space'
             } else {
@@ -37,5 +39,19 @@ class Phrase {
             // Remove life
             console.log(false);
         }
+    }
+
+    /**
+     * Displays the matched letter in the phrase
+     * @param (string) letter - letter being displayed
+     */
+    showMatchedLetter(letter) {
+        const htmlLetters = document.querySelectorAll('#phrase .letter');
+        htmlLetters.forEach(charater => {
+            if (charater.textContent === letter) {
+                charater.classList.remove('hide');
+                charater.classList.add('show');
+            }
+        });
     }
 }
