aa94a5afce5e8aa1ef0782dbccf7315972e36cf7
Jason Howie <howiejason93@gmail.com>
2019-07-15 09:36:50 -0400

    event listener to start game

--- js/app.js
+++ js/app.js
@@ -2,3 +2,8 @@
  * Project 4 - OOP Game App
  * app.js */
 const game = new Game();
+
+/**
+ * Click event on button that starts the game
+ */
+document.getElementById('btn__reset').addEventListener('click', e => game.startGame());
